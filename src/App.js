e072c45b1b0d7ca9c1ac293e96cbf84696e24254
Mickey <mickey.zheng@yahoo.com>
2021-08-25 19:33:18 -0400

    Moved stat calculation to stat component & changed logic to use React hooks instead of innerHTML and getElementById

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Table from './Components/Table';
 import Form from './Components/Form';
@@ -20,36 +20,6 @@ function App() {
 
   const jobList = (localStorage.getItem('jobAppList')) ? localStorage.getItem('jobAppList') : '[]';
   const [jobs, setJobs] = useState(jobList);
-  const jobAppList = JSON.parse(jobs)
-
-  const calcJobStats = () => {
-    let numbOfApplications = jobAppList.length
-    let daysOfJobSearching = 0;
-
-    // Calculate # rejections & rejection rate
-    let numbOfRejections = jobAppList.filter(anApp => anApp.status === 'Rejected').length;
-    let rejectionPercentage = ((numbOfRejections) / numbOfApplications) * 100
-    rejectionPercentage = (isNaN(rejectionPercentage)) ? 0 : rejectionPercentage.toFixed(2);
-
-    // Calculate # of apps still waiting on
-    let waitingApps = numbOfApplications - numbOfRejections;
-
-    // Calculate # of days job searching
-    if (jobAppList[0]) {
-      let dateToday = new Date();
-      let earliestAppDate = new Date(jobAppList[0]["date"]);
-      daysOfJobSearching = Math.floor((dateToday - earliestAppDate) / (1000 * 60 * 60 * 24)) + 1;
-      daysOfJobSearching = (isNaN(daysOfJobSearching)) ? 0 : daysOfJobSearching;
-    }
-
-    document.getElementById('jobSearchingDays').innerHTML = daysOfJobSearching;
-    document.getElementById('numbApplied').innerHTML = numbOfApplications;
-    document.getElementById('numbWaiting').innerHTML = waitingApps;
-    document.getElementById('numbRejections').innerHTML = numbOfRejections;
-    document.getElementById('rejections').innerHTML = rejectionPercentage;
-  }
-
-  useEffect(calcJobStats)
 
   return (
     <div className="App">
--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Component displays job stats: 
 // - # days since you've started job searching
@@ -7,30 +7,72 @@ import React from 'react';
 // - # job rejections
 // - % of jobs you've been rejected from
 const Stats = ({ jobs }) => {
+    const jobAppList = JSON.parse(jobs);
+    const [jobSearchingDays, setJobSearchingDays] = useState(0);
+    const [jobsApplied, setJobsApplied] = useState(0);
+    const [jobsWaiting, setJobsWaiting] = useState(0);
+    const [jobRejections, setJobRejections] = useState(0);
+    const [jobRejectionPercentage, setJobRejectionPercentage] = useState(0);
+
+    const calcJobStats = () => {
+        if (jobAppList.length === 0) { // No job apps
+            setStats(0, 0, 0, 0, 0);
+        } else {
+            let numbOfApplications = jobAppList.length;
+            let daysOfJobSearching = calcDaysJobSearching();
+            let numbOfRejections = jobAppList.filter(anApp => anApp.status === 'Rejected').length;
+            let rejectionPercentage = (((numbOfRejections) / numbOfApplications) * 100).toFixed(2);
+            let waitingApps = numbOfApplications - numbOfRejections;
+
+            setStats(daysOfJobSearching, numbOfApplications, waitingApps, numbOfRejections, rejectionPercentage);
+        }
+    }
+
+    const setStats = (daysSearching, totalJobs, jobsWaiting, jobsRejected, rejectionPercentage) => {
+        setJobSearchingDays(daysSearching);
+        setJobsApplied(totalJobs);
+        setJobsWaiting(jobsWaiting);
+        setJobRejections(jobsRejected);
+        setJobRejectionPercentage(rejectionPercentage);
+    }
+
+    const calcDaysJobSearching = () => {
+        let daysOfJobSearching = 0;
+
+        if (jobAppList.length !== 0) {
+            let dateToday = new Date();
+            let earliestAppDate = new Date(jobAppList[0]["date"]);
+            daysOfJobSearching = Math.floor((dateToday - earliestAppDate) / (1000 * 60 * 60 * 24)) + 1;
+        }
+
+        return daysOfJobSearching;
+    }
+
+    useEffect(calcJobStats);
+
     return <div id='statsContainer'>
         <h3>Job Application Stats</h3>
         <div id='jobStats'>
             <div>
                 <div># Days Searching</div>
-                <span id='jobSearchingDays'>0</span>
+                <span id='jobSearchingDays'>{jobSearchingDays}</span>
             </div>
             <div>
                 <div># Applied:</div>
-                <span id='numbApplied'>0</span>
+                <span id='numbApplied'>{jobsApplied}</span>
             </div>
             <div>
                 <div># Waiting on:</div>
-                <span id='numbWaiting'>0</span>
+                <span id='numbWaiting'>{jobsWaiting}</span>
             </div>
             <div>
                 <div># Rejections:</div>
-                <span id='numbRejections'>0</span>
+                <span id='numbRejections'>{jobRejections}</span>
             </div>
             <div>
                 <div>Rejection Rate:</div>
-                <span id='rejections'>0</span>%
+                <span id='rejections'>{jobRejectionPercentage}</span>%
             </div>
-
         </div>
     </div>
 }
