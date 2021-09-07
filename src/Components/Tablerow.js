c17f61bb9392eb292bbe56ba8d370eec90c74cf0
Mickey <mickey.zheng@yahoo.com>
2021-09-07 14:39:16 -0400

    Added new status: Unknown and cleaned up/updated Tablerow

--- src/Components/Tablerow.js
+++ src/Components/Tablerow.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 
 // Component creates an individual row in the table for a single job application
 const Tablerow = ({ jobs, appData, setJobs, setShowUpdateModal, setJobAppToUpdate }) => {
-    let { number, name, date, location, link, status, progress } = appData;
+    // Default Val for progress = 'Waiting' and for status = 'Applied'
+    const [progress, setProgress] = useState(!appData.progress ? 'Waiting' : appData.progress);
+    const [status, setStatus] = useState(!appData.status ? 'Applied' : appData.status)
     const [disableProgress, setDisableProgress] = useState(status === 'Rejected');
-    let appStatus = (status === "Applied") ? 'applied' : 'rejected';
+    let { number, name, date, location, link } = appData;
     let jobAppIndex = number - 1; // Index of job App in jobs array
     let jobList = JSON.parse(jobs);
-    progress = (progress) ? progress : 'Waiting';
 
     // Opens job application link in a new tab
     const openLink = (jobLink) => {
@@ -15,35 +16,50 @@ const Tablerow = ({ jobs, appData, setJobs, setShowUpdateModal, setJobAppToUpdat
     }
 
     const changeProgress = () => {
-        let job = jobList[jobAppIndex];
-        job.progress = (progress === 'In Progress') ? "Waiting" : 'In Progress';
-        jobList[jobAppIndex] = job;
-        updateJobList(jobList);
+        let newProgress = (progress === 'In Progress') ? 'Waiting' : 'In Progress';
+        setProgress(newProgress);
+        updateJobField('progress', newProgress);
     }
 
     const changeStatus = () => {
-        let updatedJob = jobList[jobAppIndex];
-        setDisableProgress(status === 'Applied');
-        updatedJob.status = (status === 'Rejected') ? "Applied" : "Rejected";
-        jobList[jobAppIndex] = updatedJob;
+        setDisableProgress(status === 'Unknown');
+        let newStatus = getNextStatus();
+        setStatus(newStatus);
+        updateJobField('status', newStatus);
+    }
+
+    const updateJobField = (key, newValue) => {
+        let job = jobList[jobAppIndex];
+        jobList[jobAppIndex] = job;
+        job[key] = newValue;
         updateJobList(jobList);
+    }
+
+    const getNextStatus = () => {
+        if (status === 'Applied') {
+            return 'Unknown';
+        } else if (status === 'Unknown') {
+            return 'Rejected';
+        } else {
+            return 'Applied';
+        }
     }
 
     const deleteJobApp = (number) => {
         if (!deleteConfirmed()) return;
-
-        // Remove the job app with number from the list
         let updatedJobList = jobList.filter(aJobApp => aJobApp.number !== number);
-        let jobNumber = 1;
+        updatedJobList = reIndexApps(updateJobList);
+        updateJobList(updatedJobList);
+    }
 
-        // Re-index # for each job in the list
-        updatedJobList = updatedJobList.map(aJobApp => {
-            aJobApp.number = jobNumber;
-            jobNumber++;
+    const reIndexApps = (appList) => {
+        let appNumber = 1;
+
+        return appList.map(aJobApp => {
+            aJobApp.number = appNumber;
+            appNumber++;
             return aJobApp;
         })
-
-        updateJobList(updatedJobList);
     }
 
     const deleteConfirmed = () => {
@@ -68,7 +84,7 @@ const Tablerow = ({ jobs, appData, setJobs, setShowUpdateModal, setJobAppToUpdat
     return <tr key={number}>
         <td className='edit'>
             <button className='editBttn' onClick={e => editJobApp()}>
-                <i className="fa fa-edit" aria-hidden="true"></i>
+                <i className='fa fa-edit' aria-hidden='true'></i>
             </button>
         </td>
         <td className='number'>
@@ -85,18 +101,18 @@ const Tablerow = ({ jobs, appData, setJobs, setShowUpdateModal, setJobAppToUpdat
         </td>
         <td className='link'>
             <button className='jobLinkButton' onClick={e => openLink(link)}>
-                <i className="fa fa-link" aria-hidden="true"></i>
+                <i className='fa fa-link' aria-hidden='true'></i>
             </button>
         </td>
         <td className='progress'>
             <button className='progressButton' disabled={disableProgress} onClick={e => changeProgress()}>{progress}</button>
         </td>
         <td className='status'>
-            <button className='statusButton' id={appStatus} onClick={e => changeStatus()}>{status}</button>
+            <button className={`statusButton ${status.toLowerCase()}`} onClick={e => changeStatus()}>{status}</button>
         </td>
         <td className='delete'>
             <button className='deleteButton' onClick={e => deleteJobApp(number)}>
-                <i className="fa fa-trash" aria-hidden="true"></i>
+                <i className='fa fa-trash' aria-hidden='true'></i>
             </button>
         </td>
     </tr>;
